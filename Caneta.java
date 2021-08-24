d66fb9ace20ff6d001e2346871c6a1b6ab20e315
Breno Araújo <brenoaraujor15@gmail.com>
2021-08-24 17:27:36 -0300

    Objeto Caneta - POO
    Estou criando um objeto com seus métodos.

---
+++ Caneta.java
@@ -0,0 +1,43 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status(){
+       System.out.println("Modelo: ");
+       System.out.println("Caneta de cor: ");
+       System.out.println("");
+       System.out.println();
+       System.out.println();
+       //*O "this" da preferencia/a quem está chamando o  método/*
+      
+
+    }
+     
+    void rabiscar(){
+      if(tampada){
+         System.out.print("ERRO");
+            
+        } else {
+         System.out.print("Rabisco");
+        }
+      }
+     void tampar () {
+        
+    }
+    
+      void destampar () {
+
+
+    }
+   
+
+       
+     
+
+
+    }
+    
+
